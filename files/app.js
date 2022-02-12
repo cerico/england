56aa76e8299a3568e9ef976b69425bdd4056f182
cerico <garethrobertlee@gmail.com>
2022-02-12 08:42:45 +0000

    randomize json

--- files/app.js
+++ files/app.js
@@ -8,7 +8,7 @@ let cities;
 let guessed = [];
 
 function initialize() {
-  d3.json("/files/subset.json").then(function (data) {
+  d3.json(`/files/subset.json?rand=${Math.random()}`).then(function (data) {
     cities = data
     makeMap()
   })
